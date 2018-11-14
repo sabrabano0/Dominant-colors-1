78e446e1ffdf446ffa0f61ec7cd045a7e572575a
zzpot <you@exasd.com>
2018-11-14 21:45:38 +1200

    Using commong files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "DomColors.h"
+#include "Dominant-colors/DomColors.h"
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
-#include <shlobj.h>
+#include "common.h"
 
-#pragma comment(lib, "shell32.lib")
-#pragma comment(lib, "Comdlg32.lib")
+
 
 #define WND_NAME_RGB_CUBE	"RGB cube"
 #define WND_NAME_RGB_CIE76	"RGB CIE76"
@@ -17,8 +16,6 @@
 #define WND_NAME_HSV_CIE94	"HSV CIE94"
 
 #define IMG_WIDTH	400
-
-std::string ChooseFile(HWND hWndOwner, LPCSTR pCaption);
 
 int main()
 {
@@ -67,22 +64,4 @@ int main()
 	cv::imshow(WND_NAME_HSV_CIE94, img_hsv_cie94);
 	char key = cv::waitKey(0);
 	return 0;
-}
-
-std::string ChooseFile(HWND hWndOwner, LPCSTR pCaption)
-{
-	std::string res = "";
-	OPENFILENAME ofn;
-	memset(&ofn, 0, sizeof(ofn));
-	ofn.lStructSize = sizeof(OPENFILENAME);
-	ofn.hwndOwner = hWndOwner;
-	ofn.lpstrFilter = "All files\0*\0";
-	ofn.lpstrFile = new TCHAR[MAX_PATH*2]; ofn.lpstrFile[0] = '\0';
-	ofn.nMaxFile = MAX_PATH*2-1;
-	ofn.lpstrTitle = pCaption;
-	ofn.Flags = OFN_EXPLORER | OFN_HIDEREADONLY | OFN_NOCHANGEDIR;
-	if(GetOpenFileName(&ofn))
-		res = ofn.lpstrFile;
-	delete [] ofn.lpstrFile;
-	return res;
 }
